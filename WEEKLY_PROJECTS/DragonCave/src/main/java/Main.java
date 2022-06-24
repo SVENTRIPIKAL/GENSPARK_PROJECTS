d1d2a48dfaf8e8f2dd3dba083b97b8b949231544
Jose Garza <90730468+SVENTRIPIKAL@users.noreply.github.com>
2022-06-24 15:10:59 -0500

    DRAGON CAVE - UPDATES
    -converted string cases to integers
    -added try-catch blocks for user-input
    -closed scanner

--- WEEKLY_PROJECTS/DragonCave/src/main/java/Main.java
+++ WEEKLY_PROJECTS/DragonCave/src/main/java/Main.java
@@ -7,33 +7,37 @@ public class Main {
         System.out.println("""
             You are in a land full of dragons. In front of you,
             you see two caves. In one cave, the dragon is friendly
-            and will share his treasure with you. The other dragon
-            is greedy and hungry and will eat you on sight.
+            and will share his treasure with you. In the other, the
+            dragon is greedy and hungry and will eat you on sight.
             """);
         while (!done) {
             System.out.print("Which cave will you go into? (1 or 2) ");
-            String input = scanner.nextLine();  System.out.println();
-            switch (input) {
-                case "1" -> {
-                    System.out.println("""
-                        You approach a cave...
+            try {
+                int input = Integer.parseInt(scanner.nextLine());
+                switch (input) {
+                    case 1 -> {
+                        System.out.println("""
+                        \nYou approach a cave...
                         It is dark and spooky...
                         A large dragon jumps out in front of you!
                         He opens his jaws and gobbles you down in one bite!  )=
                         """);   done = true;
-                }
-                case "2" -> {
-                    System.out.println("""
-                        You approach a mysterious cave...
+                    }
+                    case 2 -> {
+                        System.out.println("""
+                        \nYou approach a mysterious cave...
                         It glows with various colors inside...
                         A miniature dragon reveals itself to you as you near!
                         He grants you entry into the cave and to all his treasures  =D
                         """);   done = true;
-                }
-                default -> System.out.println("""
-                        Only 1 and 2 are valid! TRY AGAIN!!!  >_<'
+                    }
+                    default -> System.out.println("""
+                        \nOnly 1 and 2 are valid! TRY AGAIN!!!  >_<'
                         """);
+                }
+            } catch (Exception e) {
+                System.out.printf("%nCaught Exception: %s%n%n", e.getMessage());
             }
-        }
+        }   scanner.close();
     }
 }
